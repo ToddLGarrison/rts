2fe657151aba924aefa213bf944313685557134b
Todd Garrison <toddlgarrison@gmail.com>
2024-02-23 10:48:04 -0500

    Add guest functionality

--- src/state/GuestList.tsx
+++ src/state/GuestList.tsx
@@ -1,9 +1,26 @@
+import { useState } from "react";
+
 const GuestList: React.FC = () => {
+    const [name, setName] = useState('');
+    const [guests, setGuests] = useState<string[]>([]);
+
+    const onClick = () => {
+        setName('');
+        setGuests([...guests, name])
+    };
+
     return (
         <div>
-            Guest List
+            <h3>Guest List</h3>
+            <ul>
+                {guests.map((guest) => (
+                    <li key={guest}>{guest}</li> 
+                ))}
+            </ul>
+            <input value={name} onChange={(event) => setName(event.target.value)}/>
+            <button onClick={onClick}>Add Guest!!</button>
         </div>
-    )
-}
+    );
+};
 
 export default GuestList
